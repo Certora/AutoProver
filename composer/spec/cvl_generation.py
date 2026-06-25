1cb4178c1f6fd9740c9ba8ebee3e2609281d51c8
John Toman <jtoman@cs.washington.edu>
2026-06-25 11:48:09 -0700

    tweak prompt as suggested

--- composer/spec/cvl_generation.py
+++ composer/spec/cvl_generation.py
@@ -398,7 +398,7 @@ async def run_cvl_generator[S: CVLGenerationState, C: FeedbackToolContext, I: CV
     last_attempt = await ctx.child(LAST_ATTEMPT_KEY).cache_get(_LastAttemptCache)
     in_state_copy = in_state.copy()
     if last_attempt is not None:
-        input_copy.append("Your last working draft on this task is below; it has been put into your CVL buffer (no need to re-put it)")
+        input_copy.append("Your last working draft on this task is below; it has been automatically placed into your working CVL buffer.")
         input_copy.append(last_attempt.cvl)
         in_state_copy["curr_spec"] = last_attempt.cvl
     in_state_copy["input"] = input_copy
