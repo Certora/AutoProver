76ebc6a130919fbd3cc104a7917c718f817855ca
Eric Eilebrecht <eric@certora.com>
2026-09-22 10:31:15 -0700

    Simplify comment

--- composer/cargo/__init__.py
+++ composer/cargo/__init__.py
@@ -1,5 +1,1 @@
-"""Cargo workspace metadata and a reused compile session.
-
-Shared by Python backends that submit programs to the Prover and by Rust wheels
-that load programs into LiteSVM.
-"""
+"""Tools for interacting with Cargo builds and workspaces."""
